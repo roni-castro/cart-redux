35ac3d7bf390e85343ea0c283c8aedd6aa039f86
Roni <roni.c.castro@gmail.com>
2020-11-09 16:54:58 -0300

    Add getStockById function

---
+++ src/services/cartModels.ts
@@ -0,0 +1,10 @@
+export interface StockResponse {
+  id: number;
+  quantity: number;
+}
+
+export interface ProductResponse {
+  id: number;
+  title: string;
+  price: number;
+}
--- src/services/getProducts.ts
+++ src/services/getProducts.ts
@@ -1,6 +1,20 @@
 import api from "./api";
+import { ProductResponse, StockResponse } from "./cartModels";
 
-export async function getProducts() {
-  const response = await api.get("products");
+export async function getProducts(): Promise<ProductResponse[]> {
+  const response = await api.get<ProductResponse[]>("products");
   return response.data;
+}
+
+export async function getStocks(): Promise<StockResponse[]> {
+  const response = await api.get<StockResponse[]>("stock");
+  return response.data;
+}
+
+export async function getStockById(
+  id: number
+): Promise<StockResponse | undefined> {
+  const { data } = await api.get<StockResponse[]>("stock");
+  const productStock = data.find((stock) => stock.id === id);
+  return productStock;
 }
