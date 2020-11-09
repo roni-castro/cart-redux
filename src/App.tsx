5e192b1b1b2acf02f1261b9d5ea4eedc99a63edf
Roni <roni.c.castro@gmail.com>
2020-11-09 14:07:23 -0300

    Add Cart component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import Catalog from './components/Catalog'
+import Catalog from './components/Catalog';
+import Cart from './components/Cart';
 
 function App() {
   return (
-     <Catalog />
+    <div style={{ margin: 20 }}>
+      <Catalog />
+      <Cart />
+    </div>
   );
 }
 
---
+++ src/components/Cart.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { IStoreState } from '../store';
+import { ICartItem } from '../store/modules/cart/types';
+
+const Cart: React.FC = () => {
+  const cartItems = useSelector<IStoreState, ICartItem[]>(state => state.cart.items);
+
+  return (
+    <table style={{ textAlign: "left", marginTop: 20 }}>
+      <thead>
+        <tr>
+          <th>Produtos</th>
+          <th>Preço</th>
+          <th>Quantidade</th>
+          <th>Subtotal</th>
+        </tr>
+      </thead>
+      <tbody>
+        {cartItems.map(({ product, quantity }) => (
+          <tr key={product.id}>
+            <th>{product.title}</th>
+            <th>{product.price}</th>
+            <th>{quantity}</th>
+            <th>{(quantity * product.price).toFixed(2)}</th>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Cart;
